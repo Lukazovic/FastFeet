d6a02f1a40e321e937a5ddfe5d853b195d3ee6c0
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-05-30 12:47:40 -0300

    Add user model loader

--- backend/src/database/migrations/20200530150001-create-users.js
+++ backend/src/database/migrations/20200530150001-create-users.js
@@ -20,7 +20,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      adm: {
+      provider: {
         type: Sequelize.BOOLEAN,
         defaultValue: false,
         allowNull: false,
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Lucas',
+    email: 'lucas@email.com',
+    password_hash: '123456',
+  });
+  return res.json(user);
 });
 
 export default routes;
