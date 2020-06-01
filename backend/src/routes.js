11911ea1847cc5a70d0b4900c4329b0a3fbef648
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-06-01 15:18:19 -0300

    Create Deliverymans table + Model + Controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,19 +3,26 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
-routes.put('/recipients/:id', RecipientController.update);
 
+routes.put('/recipients/:id', RecipientController.update);
 routes.post('/recipients', RecipientController.store);
+
+routes.post('/deliveryman', DeliverymanController.store);
+routes.get('/deliveryman', DeliverymanController.index);
+routes.get('/deliveryman/:id', DeliverymanController.show);
+routes.put('/deliveryman/:id', DeliverymanController.update);
+routes.delete('/deliveryman/:id', DeliverymanController.delete);
 
 export default routes;
